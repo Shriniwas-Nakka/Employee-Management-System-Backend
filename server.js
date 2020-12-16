3aed86f34fb10df0fa5be9cc3329c0f43e48b43a
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2020-12-16 23:57:13 +0530

    ADDED: enabled cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const route = require('./Routes/routes');
 
 // create express app or initialized express
@@ -10,10 +11,13 @@ const port = 3000;
 // parsed requests of content-type - application/json
 app.use(express.json());
 
+// enable cors
+app.use(cors());
+
 // defined root routes
 app.use('/api', route)
 
-// globle exception handling
+// globle exception handling or error handler
 app.use((error, req, res, next) => {
     res.status(500).send({
         message: "Internal server error",
