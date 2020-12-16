f872df02c07b9e232a8725eef8b99c0194af05fe
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2020-12-16 17:26:01 +0530

    ADDED: created server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Welcome to Employee Management System");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+})
